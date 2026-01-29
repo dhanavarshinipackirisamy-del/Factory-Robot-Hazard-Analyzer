757167c2376a7ef236623dbd9b48f4ebf546d5ec
Dhanavarshini <dp9387@srmist.edu.in>
2026-01-29 15:09:25 +0530

    [dhana]:UC1: Display static hazard analyzer message

---
+++ src/FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,14 @@
+// UC1: Display Static Hazard Message
+// Entry point of the Factory Robot Hazard Analyzer application
+
+public class FactoryRobotHazardAnalyzer {
+
+    // main method - program execution starts here
+    public static void main(String[] args) {
+
+        // UC1 requirement:
+        // Display the system name
+        System.out.println("Factory Robot Hazard Analyzer");
+
+    }
+}
